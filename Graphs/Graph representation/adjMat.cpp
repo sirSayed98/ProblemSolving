992aabfdb4a1424038e49c7670d85901459be5f8
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-01-30 18:47:38 +0200

    add adjListWeighted.cpp

--- Graphs/Graph representation/adjMat.cpp
+++ Graphs/Graph representation/adjMat.cpp
@@ -1,11 +1,15 @@
+/** 
+ * @desc: gragh representation for adjMatrix
+*/
+
 #include <vector>
 #include <iostream>
 using namespace std;
 
-
 vector<vector<int>> graph(20, vector<int>(20, 0));
 
-int main(){
+int main()
+{
     int nVer, Edges;
     cin >> nVer >> Edges;
     for (int i = 0; i < Edges; i++)
